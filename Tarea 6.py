91c83f64416694035a7bcba3821fe54e9276bbb4
Carlos Pano Hernandez <carlospano8@gmail.com>
2017-10-17 20:48:32 -0500

    Tarea terminada. Comentarios listos y pruebas completadas con éxito. Se hicieron correcciones menores.

--- Tarea 6.py
+++ Tarea 6.py
@@ -10,41 +10,53 @@ def contarInsectos():
     print("Bienvenido al programa que registra los insectos que recolectas")
     print("")
 
-    insectos = 0#Incio del counter de insectos.
-    dias = 0#Inicio de counter de días.
-    restantes = 30#Operador para insectos faltantes.
+    insectos = 0  # Incio del counter de insectos.
+    dias = 0  # Inicio de counter de días.
+    restantes = 30  # Operador para insectos faltantes.
 
-    while insectos <= 30:
-        cantidad = int(input("¿Cuántos insectos atrapaste hoy?"))#Pregunta isnectos
+    while insectos < 30:
+        cantidad = int(input("¿Cuántos insectos atrapaste hoy?"))  # Pregunta isnectos
         dias = dias + 1
         insectos = insectos + cantidad
 
-        if insectos >= 30:#Si llega a 30
+        if insectos > 30:  # Si llega a MÁS DE 30
             print("Después de", dias, "día(s) de recolección, llevas:", insectos, "insectos.")
-            print("Te has pasado con", insectos - 30, "insectos.")#Si se pasa, da el número de insectos excedentes.
+            print("Te hace falta recolectar", insectos - 30, "insectos")  # Restante de insectos
             print("")
             print("¡Felicidades, has llegado a la meta!")
             print("")
             break
 
-        print("Después de", dias, "día(s) de recolección, llevas:", insectos, "insectos.")#Agrega los insectos y los fuciona al print.
+        if insectos == 30:  # Si llega a 30
+            print("Después de", dias, "día(s) de recolección, llevas:", insectos, "insectos.")
+            print("Te has pasado con", insectos - 30, "insectos.")  # Si se pasa, da el número de insectos excedentes.
+            print("")
+            print("¡Felicidades, has llegado a la meta!")
+            print("")
+            break
+
+        print("Después de", dias, "día(s) de recolección, llevas:", insectos,
+              "insectos.")  # Agrega los insectos y los fuciona al print.
         print("")
-        print("Te hace falta recolectar", restantes - insectos, "insectos")#Restante de insectos
+        print("Te hace falta recolectar", restantes - insectos, "insectos")  # Restante de insectos
 
 
 def escogerMayor():
     print("")
     print("Bienvenido al programa que encuentra el mayor")
 
-    elementoNuevo = 0#Elemento inicial de la lista.
+    elementoInicial = 0  # Elemento inicial de la lista.
+    mayor = [elementoInicial]  # Primer elemento
 
+    elementoNuevo = int(input("Teclea un número [-1 para salir]:"))  # Input del usuario
+    mayor.append(elementoNuevo)
     while elementoNuevo != -1:
-        mayor = [elementoNuevo]#Primer elemento
-        elementoNuevo = int(input("Teclea un número [-1 para salir]:"))#Input del usuario
-        mayor.append(elementoNuevo)#Eppend es un comando que agrega elementos a una lista en la última posición. Fué comprendido en la página: http://es.diveintopython.net/odbchelper_list.html
-        #Agrega nuevo elemento a la lista y repite el siclo si:
+        elementoNuevo = int(input("Teclea un número [-1 para salir]:"))  # Input del usuario
+        mayor.append(
+            elementoNuevo)  # Eppend es un comando que agrega elementos a una lista en la última posición. Fué comprendido en la página: http://es.diveintopython.net/odbchelper_list.html
+        # Agrega nuevo elemento a la lista y repite el siclo si:
 
-    #Esto no se cumple.
+    # Esto no se cumple.
     if elementoNuevo == -1:
         print("")
         print("El mayor es:",
@@ -55,7 +67,7 @@ def escogerMayor():
 def main():
     seleccion = 0
 
-    while seleccion >= 0:
+    while seleccion >= 0:  # Inicio de while. NOTA: SE DEBE ENTENDER EL USO DE TRUE Y FALSE PARA FUTURAS TAREAS.
         print("""Tarea 06. Ciclos While
 Autor: Carlos Pano Hernández
         
@@ -76,7 +88,8 @@ Autor: Carlos Pano Hernández
             print("")
             print("Gracias por usar este programa, regresa pronto.")
             break
-        else:
+
+        else:  # Cualquier número lo marca con error.
             print("")
             print("ERROR, teclea 1,2,3")
             print("")
